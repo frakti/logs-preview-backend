1c680f9b9ed4d80e7808325d78e2cd320bce3082
Tomasz Sikora <frakti7@gmail.com>
2017-08-09 21:02:29 +0200

    Enable json parsing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(bodyParser.text())
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
